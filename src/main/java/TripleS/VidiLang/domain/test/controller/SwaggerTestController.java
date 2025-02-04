01e4954338ef2e332af3e3f6b609e04a43be6d84
장수민 <20221416@sungshin.ac.kr>
2025-02-04 20:35:20 +0900

    chore: swagger setting

--- src/main/java/TripleS/VidiLang/domain/test/controller/SwaggerTestController.java
+++ src/main/java/TripleS/VidiLang/domain/test/controller/SwaggerTestController.java
@@ -1,4 +1,20 @@
 package TripleS.VidiLang.domain.test.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "예제 API", description = "Swagger 테스트용 API")
+@RestController
+@RequestMapping("/")
 public class SwaggerTestController {
+
+    @Operation(summary = "test API", description = "test API")
+    @GetMapping("string")
+    public String returnStr(){
+        return "Hello World";
+    }
+
 }
