d17f561e878d9272e51409fe7053ed65f9571a92
woogym <kwj9294@gmail.com>
2025-02-08 18:31:19 +0900

    docs/#3: CustonException 클래스 네이밍 추후 방안을 위한 주석 추가

--- src/main/java/TripleS/VidiLang/global/exception/model/CustomException.java
+++ src/main/java/TripleS/VidiLang/global/exception/model/CustomException.java
@@ -3,6 +3,7 @@ package TripleS.VidiLang.global.exception.model;
 import TripleS.VidiLang.global.exception.ErrorCode;
 import lombok.Getter;
 
+// 추후 서비스 이름으로 네이밍 수정
 @Getter
 public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
