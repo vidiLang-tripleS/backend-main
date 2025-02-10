6f32a64761dca48a69f1406a23577be7d63c1763
woogym <kwj9294@gmail.com>
2025-02-11 04:39:22 +0900

    feat/#4: Member 엔티티 추가

---
+++ src/main/java/TripleS/VidiLang/member/Member.java
@@ -0,0 +1,53 @@
+package TripleS.VidiLang.member;
+
+import TripleS.VidiLang.global.common.entitiy.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "USER_EMAIL")
+    private String email;
+
+    private String password;
+
+    private String nickName;
+
+    private String profileUrl;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Folder> folders;
+
+    @Builder
+    public Member(String email, String password, String nickName, String profileUrl, SocialType socialType) {
+        this.email = email;
+        this.password = password;
+        this.nickName = nickName;
+        this.profileUrl = profileUrl;
+        this.socialType = socialType;
+    }
+}
