fe4446d48761006012f1f4373d61c95c28fed6ac
woogym <kwj9294@gmail.com>
2025-02-11 06:08:07 +0900

    feat/#4: UserAnswer 엔티티 구현

--- src/main/java/TripleS/VidiLang/quiz/entity/UserAnswer.java
+++ src/main/java/TripleS/VidiLang/quiz/entity/UserAnswer.java
@@ -16,18 +16,19 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Choice {
+public class UserAnswer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String text; // 선택지 내용 -> 해당 부분 의논&리팩토링 필요함
-
-    @Column(nullable = false)
-    private boolean isCorrect; // 정답 여부
-
     @ManyToOne
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "choice_id", nullable = false)
+    private Choice choice;
+
+    @Column(nullable = false)
+    private boolean isCorrect; // 정답 여부 (미리 저장)
 }
