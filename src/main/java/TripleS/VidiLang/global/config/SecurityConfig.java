6e70d1175c6e83663ed66db29ac39d3414ad24d5
jihyeon baek <qormoon@sookmyung.ac.kr>
2025-02-16 12:23:02 +0900

    feat: 기본 Security 설정 추가 #8

--- src/main/java/TripleS/VidiLang/global/config/SecurityConfig.java
+++ src/main/java/TripleS/VidiLang/global/config/SecurityConfig.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,6 +34,26 @@ public class SecurityConfig {
 
 		return source;
 	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth
+			.requestMatchers("/", "/login", "/join", "/sign-up").permitAll()
+			.anyRequest().authenticated()
+		);
+
+		http.formLogin(FormLoginConfigurer::disable);
+		http.csrf(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+
 
 
 }
