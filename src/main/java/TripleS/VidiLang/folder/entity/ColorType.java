c0381aa07f2997d5c42b971df53f51b5d90ebb10
woogym <kwj9294@gmail.com>
2025-02-19 04:54:48 +0900

    feat/#10: ColorType converter 추가

--- src/main/java/TripleS/VidiLang/folder/entity/ColorType.java
+++ src/main/java/TripleS/VidiLang/folder/entity/ColorType.java
@@ -1,13 +1,32 @@
 package TripleS.VidiLang.folder.entity;
 
+import TripleS.VidiLang.global.exception.ErrorCode;
+import TripleS.VidiLang.global.exception.model.CustomException;
+
 public enum ColorType {
 
-    RED,
-    PINK,
-    GREEN,
-    YELLOW,
-    PURPLE,
-    BLUE,
-    SKYBLUE,
-    GRAY,
+    RED("Red"),
+    PINK("Pink"),
+    GREEN("Green"),
+    YELLOW("Yellow"),
+    PURPLE("Purple"),
+    BLUE("Blue"),
+    SKYBLUE("Skyblue"),
+    GRAY("Gray");
+
+    private final String displayCode;
+
+    ColorType(String displayCode) {
+        this.displayCode = displayCode;
+    }
+
+    public static ColorType getColorTypeOfString(String roleType) {
+        for (ColorType type : ColorType.values()) {
+            if (type.displayCode.equals(roleType)) {
+                return type;
+            }
+        }
+
+        throw new CustomException(ErrorCode.INVALID_ROLE_TYPE_EXCEPTION, ErrorCode.INVALID_TOKEN_EXCEPTION.getMessage());
+    }
 }
