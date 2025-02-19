f4ba3a431fe509c5ec9563a1ca1f23a6394be9a5
woogym <kwj9294@gmail.com>
2025-02-20 01:59:20 +0900

    feat/#10: folder 수정 로직 추가

--- src/main/java/TripleS/VidiLang/folder/service/FolderService.java
+++ src/main/java/TripleS/VidiLang/folder/service/FolderService.java
@@ -1,7 +1,7 @@
 package TripleS.VidiLang.folder.service;
 
 import TripleS.VidiLang.folder.dto.reponse.FolderResponse;
-import TripleS.VidiLang.folder.dto.request.FolderCreateRequest;
+import TripleS.VidiLang.folder.dto.request.FolderRequest;
 import TripleS.VidiLang.folder.entity.ColorType;
 import TripleS.VidiLang.folder.entity.Folder;
 import TripleS.VidiLang.folder.entity.LanguageType;
@@ -26,16 +26,16 @@ public class FolderService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public void createFolder(Principal principal, FolderCreateRequest folderCreateRequest) {
+    public void createFolder(Principal principal, FolderRequest folderRequest) {
         Long memberId = Long.parseLong(principal.getName());
         Member member = getMemberById(memberId);
-        Folder folder = saveFolder(member, folderCreateRequest);
+        Folder folder = toFolderWithMember(member, folderRequest);
 
         folderRepository.save(folder);
     }
 
     @Transactional(readOnly = true)
-    public ApiResponseTemplate<List<FolderResponse>> getFolderListByPrincipal(Principal principal) {
+    public ApiResponseTemplate<List<FolderResponse>> getFolderList(Principal principal) {
         Long memberId = Long.parseLong(principal.getName());
         exitsMemberById(memberId);
         List<Folder> folders = findFolderListByMemberId(memberId);
@@ -47,6 +47,24 @@ public class FolderService {
                 .message("폴더 조회 성공")
                 .data(folderResponses)
                 .build();
+    }
+
+    @Transactional
+    public void updateFolderBy(Principal principal, FolderRequest folderRequest) {
+        Long memberId = Long.parseLong(principal.getName());
+        exitsMemberById(memberId);
+        Folder folder = findFolderById(folderRequest.folderId());
+
+        folder.update(
+                folderRequest.name(),
+                ColorType.getColorTypeOfString(folderRequest.colorType()),
+                LanguageType.getLanguageTypeOfString(folderRequest.languageType()));
+    }
+
+    private Folder findFolderById(Long folderId) {
+        return folderRepository.findById(folderId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ID_EXCEPTION,
+                        ErrorCode.NOT_FOUND_ID_EXCEPTION.getMessage() + "id = " + folderId));
     }
 
     private void exitsMemberById(Long memberId) {
@@ -72,12 +90,12 @@ public class FolderService {
                         ErrorCode.INVALID_ID_EXCEPTION.getMessage() + memberId));
     }
 
-    private Folder saveFolder(Member member, FolderCreateRequest folderCreateRequest) {
+    private Folder toFolderWithMember(Member member, FolderRequest folderRequest) {
         return Folder.builder()
                 .member(member)
-                .name(folderCreateRequest.getName())
-                .colorType(ColorType.getColorTypeOfString(folderCreateRequest.getColorType()))
-                .languageType(LanguageType.getLanguageTypeOfString(folderCreateRequest.getLanguageType()))
+                .name(folderRequest.name())
+                .colorType(ColorType.getColorTypeOfString(folderRequest.colorType()))
+                .languageType(LanguageType.getLanguageTypeOfString(folderRequest.languageType()))
                 .build();
     }
 }
