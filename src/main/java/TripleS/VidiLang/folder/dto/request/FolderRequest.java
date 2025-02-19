cc2e1e18e1ab79764d6fb3cc73a51bfb5dec5f85
woogym <kwj9294@gmail.com>
2025-02-20 05:18:48 +0900

    refactor/#10: id필드 삭제 / 파라미터 분리

--- src/main/java/TripleS/VidiLang/folder/dto/request/FolderRequest.java
+++ src/main/java/TripleS/VidiLang/folder/dto/request/FolderRequest.java
@@ -5,10 +5,8 @@ import lombok.Builder;
 
 @Builder
 public record FolderRequest(
-        @Schema(description = "폴더Id", example = "1") Long folderId,
         @Schema(description = "폴더명", example = "나의 불어 노트") String name,
         @Schema(description = "색상 타입 - 파스칼 케이스", example = "Red") String colorType,
         @Schema(description = "언어 타입 - 파스칼 케이스", example = "French") String languageType
 ) {
-
 }
